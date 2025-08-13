d37a3cb89c66d56e587bfebac74937fe28b4ac8c
DesarrolladorJuanCTorres <jctorresdelcastillo@gmail.com>
2025-08-13 12:23:52 -0500

    cambios footer

--- resources/views/general/footer.blade.php
+++ resources/views/general/footer.blade.php
@@ -2,7 +2,9 @@
 <div class="container-fluid bg-footer text-secondary mt-5 pt-5">
     <div class="row px-xl-5 pt-5">
         <div class="col-lg-4 col-md-12 mb-5 pr-3 pr-xl-5">
-            <img class="py-4" width="200" src="{{asset("storage/$business->image")}}" alt="">
+            <a href="/">
+                <img class="py-4" width="200" src="{{asset("storage/$business->image")}}" alt="">
+            </a>            
             <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>{{$business->address}}</p>
             <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>{{$business->email}}</p>
             <p class="mb-0"><i class="fa fa-phone-alt text-primary mr-3"></i>{{$business->phone}}</p>
