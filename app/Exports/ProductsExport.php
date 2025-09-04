7704035da10110cc82b4abc52590f6a02ebd74c2
DesarrolladorJuanCTorres <jctorresdelcastillo@gmail.com>
2025-09-04 10:44:52 -0500

    cambios varios

--- app/Exports/ProductsExport.php
+++ app/Exports/ProductsExport.php
@@ -11,12 +11,33 @@ class ProductsExport implements FromCollection, WithHeadings
     public function collection()
     {
         // Devuelve todos los registros de la tabla products
-        return DB::table('products')->get();
+        return DB::table('products')
+            ->join('brands', 'products.brand_id', '=', 'brands.id')
+            ->join('taxonomies', 'products.taxonomy_id', '=', 'taxonomies.id')
+            ->select(
+                'products.id',
+                'products.name',
+                'brands.name as brand_name',
+                'taxonomies.name as taxonomy_name',
+                'products.price',
+                'products.stock',
+                'products.created_at',
+                'products.updated_at'
+            )->get();
     }
 
     public function headings(): array
     {
         // Trae dinámicamente los nombres de las columnas
-        return DB::getSchemaBuilder()->getColumnListing('products');
+        return [
+            'ID',
+            'Producto',
+            'Marca',
+            'Categoria',
+            'Precio',
+            'Stock',
+            'Creado',
+            'Actualizado',
+        ];
     }
 }
