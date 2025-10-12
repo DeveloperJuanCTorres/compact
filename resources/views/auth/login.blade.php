d770b7ffd631898f6992ef8fbf842662c85d1aa8
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2025-10-12 17:44:02 -0500

    cambios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,7 +38,9 @@
       </div>
       <div class="box-root padding-top--24 flex-flex flex-direction--column" style="flex-grow: 1; z-index: 9;">
         <div class="box-root padding-top--48 padding-bottom--24 flex-flex flex-justifyContent--center">
-          <img src="img/logo.png" width="300" alt="CompactSeguridad">
+          <a href="/">
+            <img src="img/logo.png" width="300" alt="CompactSeguridad">
+          </a>
         </div>
         <div class="formbg-outer">
           <div class="formbg">
