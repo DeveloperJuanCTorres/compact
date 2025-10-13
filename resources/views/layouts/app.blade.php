096d7f9e7589097c3763e07ce86e03905e6d8c3c
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2025-10-13 16:13:09 -0500

    version

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,7 @@
 
     <!-- Libraries Stylesheet -->
      <?php
-        $version = '1993.3.7';
+        $version = '1993.3.8';
     ?>
 
     <link href="{{asset('css/wpp.css')}}?v=<?php echo $version ?>" rel="stylesheet">
