86ca097881b7966dc0caaa3e77b7a49b4474ff32
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2026-03-13 12:49:55 -0500

    cam

--- resources/views/tienda/ofertas.blade.php
+++ resources/views/tienda/ofertas.blade.php
@@ -93,7 +93,7 @@
 
 <!-- Carousel Start -->
 <div class="banner">
-    <img src="img/banner-oferta-2.png" alt="Banner" class="banner-img">
+    <img src="img/ofertas-banner-2.png" alt="Banner" class="banner-img">
 </div>
 
 <style>
